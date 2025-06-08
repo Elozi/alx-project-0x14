5a4e0ecc659b0cd4a9e06a5ff7bc3d7ecf090fa0
Abigail Ogbogo <101398676+Elozi@users.noreply.github.com>
2025-06-08 19:15:38 +0100

    Update Button.tsx

--- alx-project-0x03/components/common/Button.tsx
+++ alx-project-0x03/components/common/Button.tsx
@@ -1,5 +1,5 @@
 
-import { ButtonProps}  from "@/interface/index";
+import { ButtonProps } from "@/interface";
 
 const Button = ({ buttonLabel, buttonSize, buttonBackgroundColor, action }: ButtonProps) => {
 
