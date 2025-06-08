fe850441e861ea10b7efc5ab89f154fc64e4d836
Elozi <Abbyjamezzz@gmail.com>
2025-06-08 18:36:20 +0100

    finalised

---
+++ alx-project-0x03/components/common/Button.tsx
@@ -0,0 +1,18 @@
+interface ButtonProps {
+  action: () => void;
+  buttonLabel: string;
+  buttonBackgroundColor: string;
+}
+
+const Button = ({ action, buttonLabel, buttonBackgroundColor }: ButtonProps) => {
+  return (
+    <button
+      onClick={action}
+      className={`bg-${buttonBackgroundColor}-500 hover:bg-${buttonBackgroundColor}-600 text-white font-semibold py-2 px-4 rounded`}
+    >
+      {buttonLabel}
+    </button>
+  );
+};
+
+export default Button;
