b5355a5ac8ceac6d7855a9ac96bc8f027b5012b0
Elozi <Abbyjamezzz@gmail.com>
2025-06-08 19:10:52 +0100

    fixed bugs

--- alx-project-0x03/interface/index.tsx
+++ alx-project-0x03/interface/index.tsx
@@ -21,10 +21,11 @@ export interface CardProps {
   export interface PageRouteProps {
     pageRoute: string;
   }
-  export interface ButtonProps {
-    action: () => void;
-    buttonLabel: string;
-    buttonBackgroundColor: string;
+ export interface ButtonProps {
+    buttonLabel: string
+    buttonSize?: string
+    buttonBackgroundColor?: 'red' | 'blue' | 'orange' | 'green'
+    action?: () => void
   }
   export interface LayoutProps {
     children: ReactNode;
